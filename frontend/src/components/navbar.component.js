1b0eb9ead1c2b66c34baeb662f1f36546d517bed
narainkarthikv <narainkarthik812@gmail.com>
2023-03-08 16:08:44 +0530

    components updated

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -4,20 +4,20 @@ import { Link} from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      <Link className="navbar-brand" to="/">Fitness-Tracker</Link>
+      <Link className="navbar-brand" to="https://tiny-seahorse-4a2b7e.netlify.app/">Fitness-Tracker</Link>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
             {/* <Link exact className="nav-link" to="/">Exercises</Link> */}
-            <a className="nav-link" href="/">Exercises</a>
+            <a className="nav-link" href="https://tiny-seahorse-4a2b7e.netlify.app/">Exercises</a>
           </li>
           <li className="nav-item">
             {/* <Link className="nav-link" to="/create">Create Exercise Log</Link> */}
-            <a className="nav-link" href="/create">Create Exercise Log</a>
+            <a className="nav-link" href="https://tiny-seahorse-4a2b7e.netlify.app/create">Create Exercise Log</a>
           </li>
           <li className="nav-item">
             {/* <Link className="nav-link" to="/user">Create User</Link> */}
-            <a className="nav-link" href="/user">Create User</a>
+            <a className="nav-link" href="https://tiny-seahorse-4a2b7e.netlify.app/user">Create User</a>
           </li>
         </ul>
       </div>
